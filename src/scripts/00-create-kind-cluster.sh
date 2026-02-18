a7d891c2463858c82243f98e1d511bde60e02a52
Adriana Villela <50256412+avillela@users.noreply.github.com>
2026-02-18 20:35:11 +0000

    Update scripts

--- src/scripts/00-create-kind-cluster.sh
+++ src/scripts/00-create-kind-cluster.sh
@@ -1,7 +1,21 @@
 #! /bin/bash
 
+# Pass in the name of the .env file
+ENVFILE=$1
+
+# Load environment variables from .env file
+if [[ -n "${ENVFILE}" && -f ${ENVFILE} ]]; then
+  echo "*** Loading environment variables from .env..."
+  export $(grep -v '^#' ${ENVFILE} | xargs)
+  echo "Environment variables loaded."
+else
+  echo "*** No ${ENVFILE} file found in the current directory. Exiting."
+  exit 1
+fi
+
+# Workaround for running KinD in a dev container (might be fixed by now)
 docker network create -d=bridge -o com.docker.network.bridge.enable_ip_masquerade=true -o com.docker.network.driver.mtu=1500 --subnet fc00:f853:ccd:e793::/64 kind
 
-kind create cluster --name otel-demo
+kind create cluster --name $K8S_CLUSTER_NAME
 
 # kind delete clusters otel-demo
--- src/scripts/03-deploy-resources.sh
+++ src/scripts/03-deploy-resources.sh
@@ -1,7 +1,33 @@
 #! /bin/bash
 
+# Pass in the name of the .env file
+ENVFILE=$1
+
+# Load environment variables from .env file
+if [[ -n "${ENVFILE}" && -f ${ENVFILE} ]]; then
+  echo "*** Loading environment variables from .env..."
+  export $(grep -v '^#' ${ENVFILE} | xargs)
+  echo "Environment variables loaded."
+else
+  echo "*** No ${ENVFILE} file found in the current directory. Exiting."
+  exit 1
+fi
+
 kubectl apply -f src/k8s/namespace.yaml
-kubectl apply -f src/k8s/otel-collector-secret-dt.yaml
+
+# Create secret for configuring OTel Collector
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: otel-collector-secret
+  namespace: otel-demo
+type: Opaque
+stringData:
+  DT_TOKEN: $DT_API_TOKEN
+  DT_ENV: $DT_URL
+EOF
+
 kubectl apply -f src/k8s/otel-collector-rbac.yaml
 kubectl apply -f src/k8s/ta-rbac.yaml
 kubectl apply -f src/k8s/otel-collector-dt.yaml
